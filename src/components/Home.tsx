12156a8448087fde553b2935a74834f045eb826e
yy695575613 <yynhexsh@163.com>
2024-10-12 06:26:45 +0000

    update home
    Signed-off-by: yy695575613 <yynhexsh@163.com>

---
+++ src/components/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./Home.css";
+import "./Home.less";
+import "./Home.scss";
+
+function Home() {
+    return (
+        <div>
+            <h1>React Home</h1>
+        </div>
+    );
+}
+
+export default Home;
