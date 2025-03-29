847ccc84174d424e977d66e90d4bf37b254db9ae
yy695575613 <yynhexsh@163.com>
2025-03-29 23:24:56 +0800

    webpack优化

--- webpack.prod.js
+++ webpack.prod.js
@@ -8,7 +8,7 @@ const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = merge(config, {
     mode: "production",
-    devtool: false,
+    devtool: 'source-map',
     plugins: [
         new Dotenv({
             path: path.resolve(__dirname, '.env.production'), // 加载生产环境变量
@@ -19,6 +19,7 @@ module.exports = merge(config, {
     parallelism: 2, // 限制并行数
     cache: false, // 禁用缓存
     optimization: {
+        runtimeChunk: 'single', // 新增运行时分离
         minimizer: [
             new TerserPlugin({
                 parallel: 2, // 限制压缩线程
@@ -29,16 +30,5 @@ module.exports = merge(config, {
                 },
             }),
         ],
-        splitChunks: {
-            chunks: 'all',
-            minSize: 20000, // 提升分块阈值
-            cacheGroups: {
-                vendors: {
-                    test: /[\\/]node_modules[\\/]/,
-                    priority: -10,
-                    reuseExistingChunk: true
-                }
-            }
-        }
     },
 });
